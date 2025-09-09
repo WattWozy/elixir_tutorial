f2d7d7399f657e762e06fb404b5da3c9aaab3413
niklasWozniak <niklas.wozniak@tietoevry.com>
2025-09-09 17:17:47 +0200

    flattening lists

--- first_project/cards/lib/cards.ex
+++ first_project/cards/lib/cards.ex
@@ -1,11 +1,24 @@
 defmodule Cards do
   
   def create_deck do
-    ["Ace", "Two", "Three"] # creating a list
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    
+    cards = for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
+    end
+
+    List.flatten(cards) # flattening a list of nested lists into a single list
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck) # shuffle a list
+  end
+
+  def contains?(deck, card) do  # the question mark is a convention for functions that return boolean values
+    Enum.member?(deck, card) # check if a list contains an element
   end
 
 end
