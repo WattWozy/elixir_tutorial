7a182cd5b73f6c1dd3ba1b450e72ec04731db232
nikwoz <niklaswozlop@gmail.com>
2025-09-14 18:04:36 +0200

    some functions to refresh recursion

---
+++ book_practice/functions/Times.exs
@@ -0,0 +1,11 @@
+defmodule Times do 
+
+  def double(n) do
+    n * 2
+  end
+
+  def triple(n), do: n*3
+
+  def cuadruple(n), do: double(double(n))
+
+end
---
+++ book_practice/functions/sum.exs
@@ -0,0 +1,6 @@
+defmodule Sum do
+
+  def all(0), do: 0
+  def all(n), do: n+all(n-1)
+
+end
